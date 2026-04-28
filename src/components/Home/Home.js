c70dc8c243f16dc86fe38390d4557294e58fd23f
Shivam Bansal <bansal.shivam1216@gmail.com>
2026-04-28 16:30:22 +0530

    Changes made

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,9 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import homeLogo_2 from "../../Assets/me.png"; 
+import { Col, Container, Row } from "react-bootstrap";
+import Tilt from "react-parallax-tilt";
+import homeLogo_2 from "../../Assets/me.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
-import Tilt from "react-parallax-tilt";
 
 function Home() {
   return (
