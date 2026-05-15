c244eb3be61b9a82680c3e061518da366d2b2148
Shivam Bansal <bansal.shivam1216@gmail.com>
2026-05-15 07:35:00 +0530

    SEO Changes made

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import { Col, Container, Row } from "react-bootstrap";
 import Tilt from "react-parallax-tilt";
-import homeLogo_2 from "../../Assets/me.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
@@ -25,20 +24,46 @@ function Home() {
                 <strong className="main-name"> Shivam Bansal</strong>
               </h1>
 
+              <h1
+                aria-hidden="false"
+                style={{
+                  position: "absolute",
+                  left: "-10000px",
+                  top: "auto",
+                  width: "1px",
+                  height: "1px",
+                  overflow: "hidden"
+                }}
+              >
+                Shivam Bansal - Freelance Web Developer & Technical Trainer
+              </h1>
+
               <div style={{ padding: 50, textAlign: "left" }}>
                 <Type />
               </div>
             </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
-            <Tilt>
-              <img
-                src={homeLogo_2}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px", borderRadius: '50px' }}
-              />
-            </Tilt>
+              <Tilt>
+                {/* <img
+                  // src={homeLogo_2}
+                  src="/images/shivam_bansal.png"
+                  alt="Shivam Bansal - Freelance Web Developer and Technical Trainer"
+                  className="img-fluid"
+                  style={{ maxHeight: "450px", borderRadius: '50px' }}
+                /> */}
+
+                <img
+                  src="/images/shivam_bansal.png"
+                  alt="Shivam Bansal - Freelance Web Developer and Technical Trainer"
+                  className="img-fluid"
+                  loading="eager"
+                  fetchpriority="high"
+                  width="450"
+                  height="450"
+                  style={{ maxHeight: "450px", borderRadius: "50px" }}
+                />
+              </Tilt>
             </Col>
           </Row>
         </Container>
