3519219cf679623194e0b9e2e200c8260088c256
Shivam Bansal <bansal.shivam1216@gmail.com>
2026-05-17 12:22:55 +0530

    Changes made in SEO

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -15,9 +15,9 @@ function Home2() {
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
+            <h2 style={{ fontSize: "2.6em" }}>
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
-            </h1>
+            </h2>
             <p className="home-about-body">
               I fell in love with programming and I have at least learnt
               something, I think… 🤷‍♂️
@@ -60,7 +60,7 @@ function Home2() {
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
+            <h2>FIND ME ON</h2>
             <p>
               Feel free to <span className="purple">connect </span>with me
             </p>
